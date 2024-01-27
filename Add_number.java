496d863ab28eaef9161aa1dfa8b4679c88fb8f14
Danya01 <40769550+Danya01@users.noreply.github.com>
2024-01-28 00:55:42 +0530

    Add files via upload

---
+++ Add_number.java
@@ -0,0 +1,13 @@
+public class Add_number {
+    public static void main(String[] args) {
+        int a = 15;
+        int b= 3;
+        int c;
+
+        c = a + b;
+        System.out.println( "The values of c is " + c );
+
+
+    }
+    
+}
