769661420ceb34720603060ea32eaa07decace76
Danya01 <40769550+Danya01@users.noreply.github.com>
2024-01-28 01:03:11 +0530

    Add files via upload

---
+++ Floating_number_product.java
@@ -0,0 +1,14 @@
+public class Floating_number_product {
+
+    public static void main (String args[])
+    {
+        float a = 2.4f;
+        float b = 1.6f;
+
+        float c = a * b ;
+
+        System.out.println("The product is " +  c );
+
+
+    }
+}
